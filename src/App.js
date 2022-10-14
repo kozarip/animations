1d4221c5ffe59d36eefa81f5f77bc5cd9bb9908a
Peter Kozari <kozaripeti@gmail.com>
2022-10-14 10:26:23 +0200

    fine tuning

--- src/App.js
+++ src/App.js
@@ -25,7 +25,7 @@ function App() {
         currentImageNumber = 0
       }
       setCurrentImage(images[currentImageNumber])
-    }, 4500)
+    }, 4000)
 
     //circle images
     const t3 = gsap.timeline({ repeat: -1, delay: 2 });
